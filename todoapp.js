215692cabe0b03eaa0f1601e560bbc4f0c5ab12d
Vanessa Gutierrez <vanessagutierrez93@msn.com>
2021-01-22 20:49:36 -0600

    To Do App

---
+++ todoapp.js
@@ -0,0 +1,59 @@
+// Vanessa Gutierrez 01/22/2021
+
+const todos = [];
+
+const pendingClasses =
+  "bg-white w-full text-center text-pink-600 rounded py-4 border-2 border-pink-600 transition transform ease-in-out duration-300 hover:bg-pink-700 hover:text-white hover:scale-110 hover:rotate-1 cursor-pointer";
+const completedClasses =
+  "bg-white w-full text-center text-blue-600 rounded py-4 border-2 border-blue-600 transition transform ease-in-out duration-300 hover:bg-blue-700 hover:text-white hover:scale-110 hover:-rotate-1 cursor-pointer";
+
+const pendingList = document.getElementById("pendingList");
+const completedList = document.getElementById("completedList");
+
+const showTodos = () => {
+  const pendingTodos = todos.filter((todo) => todo.status === "pending");
+
+  pendingList.innerHTML = "";
+  pendingTodos.forEach((todo) => {
+    const pendingItem = document.createElement("li");
+    pendingItem.className = pendingClasses;
+    pendingItem.innerText = todo.text;
+    pendingItem.id = todo.id;
+    pendingList.appendChild(pendingItem);
+  });
+
+  const completedTodos = todos.filter((todo) => todo.status === "done");
+
+  completedList.innerHTML = "";
+  completedTodos.forEach((todo) => {
+    const completedItem = document.createElement("li");
+    completedItem.className = completedClasses;
+    completedItem.innerText = todo.text;
+    completedItem.id = todo.id;
+    completedList.appendChild(completedItem);
+  });
+};
+
+const addForm = document.getElementById("addForm");
+const newTodo = document.getElementById("newTodo");
+
+addForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  todos.push({
+    id: Math.floor(Math.random() * 100000).toString(),
+    text: newTodo.value,
+    status: "pending",
+  });
+  newTodo.value = "";
+  showTodos();
+});
+
+pendingList.addEventListener("click", (event) => {
+  todos.find((todo) => todo.id === event.target.id).status = "done";
+  showTodos();
+});
+
+completedList.addEventListener("click", (event) => {
+  todos.find((todo) => todo.id === event.target.id).status = "pending";
+  showTodos();
+});
